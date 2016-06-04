92156cdbb0009a3d0e090c964f8edbfacb4b4743
csstudent <csstudent@WebDev23.BoiseState.edu>
2016-06-04 11:07:21 -0600

    intergrating

---
+++ blog.php
@@ -0,0 +1,33 @@
+<?php include("header.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>UNI-que Blog</title>
+  <meta name="description" content="This is a blog for UNI-que members" />
+  <meta name="keywords" content="Fun, Awesomeness, Help" />
+  <link href="images/favicon.ico" type="image/x-icon" rel="shortcut icon" />
+  <link href="css/blog-style.css" type="text/css" rel="stylesheet" />
+</head>
+<body>
+  <img id="logo" src="images/logo.png" alt="logo" />
+  <h1 id="welcome-message">Welcome to our blog. </h1>
+  <h2>UNI-que is happening</h2>
+
+  <p class="important">  
+  </p>
+
+  <h2>Things are happening in Boise &hearts;</h2>
+  <p>
+    Muffin <em>tootsie roll wafer wafer souffl&eacute; sugar plum topping caramels candy canes. Tart oat cake danish.
+    <strong>Candy canes</strong></em> halvah jelly beans gummies. Carrot cake I love sweet cheesecake croissant
+    sugar plum. I love brownie sugar plum candy apple pie cotton candy croissant candy canes sugar
+    plum. Chocolate bar cookie jelly-o brownie cupcake pie bear claw. Jelly pastry dessert wafer gingerbread bear
+    claw ice cream icing. Cheesecake carrot cake tart caramels I love sesame snaps gummi bears cake. I love halvah
+    soufflé cheesecake.
+  </p>
+
+  <h3>Things and stuff</h3><br>
+
+  <?php include("footer.html");?>
+  </body>
+</html>
