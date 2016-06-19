be6ca65827fa5e734488616dc4064e38312dcd7e
chokett <chrishokett@u.boisestate.edu>
2016-06-18 18:54:50 -0600

    community

--- community.php
+++ community.php
@@ -1,20 +1,20 @@
 <?php $thisPage = 'Network Community'; ?>
 <?php require_once('includes/header.php'); ?>
-<?php require_once('includes/navigation.php'); 
-require_once "Dao.php";
-?>
-  <div class="content">  
-  	<h1>Welcome to the Network Community page.</h1>
+<?php require_once('includes/navigation.php'); ?> 
+<?php require_once ('includes/Dao.php'); ?>
+  
+
+
+	<h1>Welcome to the Network Community page.</h1>
 	
 	<h2>UNI-que member community &hearts;</h2>
 	<p>
 	<html>
   <head>
     <title>List of Comments</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
   </head>
   <body>
-    <form name="commentForm" action="handler.php" method="POST">
+    <form name="commentForm" action="community_handler.php" method="POST">
       <div>
         <label for="comment">Leave a comment:</label>
         <input type="text" id="comment" name="comment">
